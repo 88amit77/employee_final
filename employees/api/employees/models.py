ccbc1820f5d105f3db47766cd74e7bb6f7002e02
amit <88amit77@gmail.com>
2020-05-21 08:55:59 +0530

    code added

--- employees/api/employees/models.py
+++ employees/api/employees/models.py
@@ -1,1 +1,137 @@
 from django.db import models
+
+
+class Employee(models.Model):
+    emp_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=30)
+    dob = models.DateField()
+    gender = models.CharField(max_length=10)
+    blood_group = models.CharField(max_length=10)
+    marital_status = models.BooleanField(default=False)
+    marriage_anniversary = models.DateField()
+    official_email = models.EmailField()
+    personal_email = models.EmailField()
+    official_number = models.CharField(max_length=10)
+    personal_number = models.CharField(max_length=10)
+    leave_approval_user_id = models.IntegerField()
+    facebook = models.CharField(max_length=50)
+    instagram = models.CharField(max_length=50)
+    linkedin = models.CharField(max_length=50)
+    twitter = models.CharField(max_length=50)
+    date_of_joining = models.DateField()
+    probation_period = models.IntegerField()
+    Current_address_line1 = models.CharField(max_length=50)
+    Current_address_line2 = models.CharField(max_length=50)
+    current_country = models.CharField(max_length=15)
+    current_state = models.CharField(max_length=10)
+    current_pincode = models.CharField(max_length=15)
+    current_house_type = models.CharField(max_length=15)
+    Current_staying_since = models.DateField()
+    Current_city = models.CharField(max_length=25)
+    Permanent_address_line1 = models.CharField(max_length=50)
+    Permanentt_address_line2 = models.CharField(max_length=50)
+    Permanent_country = models.CharField(max_length=15)
+    Permanent_state = models.CharField(max_length=15)
+    Permanent_pincode = models.CharField(max_length=15)
+    Employee_type = models.CharField(max_length=15)
+    Employee_Status = models.BooleanField(default=False)
+    Job_title = models.CharField(max_length=30)
+    termination_date = models.DateField()
+    work_location_add = models.CharField(max_length=20)
+    designation = models.CharField(max_length=20)
+    department = models.CharField(max_length=20)
+    resignation_date = models.DateField()
+    resignation_notes = models.CharField(max_length=50)
+    notice_date = models.DateField()
+    notice_period = models.IntegerField()
+    bank_acc_number = models.CharField(max_length=30)
+    ifsc_code = models.CharField(max_length=15)
+    bank_name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
+class Documents(models.Model):
+    documents = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name='documents_emp', null=True,
+                                  blank=True)
+    document_id = models.AutoField(primary_key=True, )
+    pan_number = models.CharField(max_length=20)
+    pan_card = models.FileField()
+    address_proof = models.FileField()
+    permanent_proof = models.FileField()
+    aadharcard_number = models.CharField(max_length=20)
+    aadharcard = models.FileField()
+
+
+class Education(models.Model):
+    educations = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name='educations_emp', null=True,
+                                   blank=True)
+    education_id = models.AutoField(primary_key=True)
+    institute_name = models.CharField(max_length=50)
+    course_type = models.CharField(max_length=30)
+    Stream = models.CharField(max_length=30)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    average_marks = models.FloatField()
+    verified = models.BooleanField(default=False)
+
+
+class FamilyMembers(models.Model):
+    family_members = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name='family_members_emp', null=True,
+                                       blank=True)
+    family_member_id = models.AutoField(primary_key=True)
+    family_Member_name = models.CharField(max_length=20)
+    relation = models.CharField(max_length=20)
+    contact_number = models.CharField(max_length=10)
+
+
+class WorkHistory(models.Model):
+    work_historys = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name='work_historys_emp', null=True,
+                                      blank=True)
+    work_history_id = models.AutoField(primary_key=True)
+    company_name = models.CharField(max_length=30)
+    period_from = models.DateField()
+    period_to = models.DateField()
+    designation = models.CharField(max_length=20)
+    reason_for_leaving = models.CharField(max_length=50)
+    verified = models.BooleanField(default=False)
+
+
+class LeaveRules(models.Model):
+    leave_id = models.AutoField(primary_key=True)
+    leave_name = models.CharField(max_length=50)
+    interval_months = models.PositiveIntegerField()
+    add_value = models.FloatField()
+    yearly_carry_forward = models.BooleanField(default=False)
+    document_required = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.leave_name
+
+
+class EmpLeaveId(models.Model):
+    emp_leave_id = models.AutoField(primary_key=True)
+    emp_id = models.ForeignKey(Employee, related_name='employee11', on_delete=models.CASCADE, default=None, unique=False)
+    leave_id = models.ManyToManyField(LeaveRules)
+
+
+class EmpLeaveApplied(models.Model):
+    ACTIVITY_TYPES = (
+        ('APPROVED', 'approved'),
+        ('PENDING', 'pending'),
+        ('REJECT', 'reject'),
+    )
+    emp_leave_app_id = models.AutoField(primary_key=True)
+    emp_id = models.ForeignKey(Employee, related_name='employee', on_delete=models.CASCADE, default=None, unique=False)
+    leave_id = models.ForeignKey(LeaveRules, on_delete=models.CASCADE, default=None, unique=False)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    status = models.CharField(max_length=10, choices=ACTIVITY_TYPES, default="PENDING")
+    reason = models.CharField(max_length=50, null=True, blank=True)
+    action_by = models.ForeignKey(Employee, related_name='action_by', on_delete=models.CASCADE, default=None,
+                                  unique=False)
+
+    def __str__(self):
+        return self.reason
+
--- employees/api/employees/views.py
+++ employees/api/employees/views.py
@@ -1,10 +1,466 @@
 from rest_framework import viewsets
+import csv
+from django.views.generic import View
+from django.http import HttpResponse
+from django.db.models import Q
+from rest_framework import status
+import requests
 from rest_framework.response import Response
 from rest_framework.pagination import PageNumberPagination
-from rest_framework.views import APIView
-import requests
+from .models import (Employee, Education, Documents, FamilyMembers, WorkHistory, LeaveRules, EmpLeaveApplied, EmpLeaveId)
+from .serializers import (
+      EmployeeSerializer,
+      ListEmployeeSerializer,
+      EducationSerializer,
+      DocumentsSerializer,
+      FamilyMembersSerializer,
+      WorkHistorySerializer,
+      EmployeeColumnModelSerializer,
+      DynamicFieldsModelSerializer,
+      LeaveRulesSerializer,
+      Employee1Serializer,
+      ListEmployee1Serializer,
+      EmpLeaveAppliedSerializer,
+      EmpLogSerializer,
+      EmployeenewSerializer,
+      Employee2Serializer,
+)
+
+DEFAULT_PAGE = 1
 
 
-class CustomPageNumberPagination(PageNumberPagination):
+class CustomPagination(PageNumberPagination):
+    page = DEFAULT_PAGE
+    page_size = 20
     page_size_query_param = 'page_size'
 
+    def get_paginated_response(self, data):
+        return Response({
+            'links': {
+                'next': self.get_next_link(),
+                'previous': self.get_previous_link()
+            },
+            'total': self.page.paginator.count,
+            'page': int(self.request.GET.get('page', DEFAULT_PAGE)),
+            'page_size': int(self.request.GET.get('page_size', self.page_size)),
+            'UI_data': {
+                'sticky_headers': [
+                                'emp_id',
+                                'name',
+                                         ],
+                'header': {
+                             'emp_id': 'Employee Id',
+                             'name': 'Employee Name',
+                             'Permanent_address_line1': "Address",
+                             "designation": 'Designation',
+                             "gender": "Gender",
+                             "official_email": 'Official Email',
+                             "date_of_joining": 'Date of Joining',
+                             'department': "Department",
+                             "official_number": 'Phone',
+                             'dob': 'Date of Birth',
+                             "work_location_add": 'Work Location',
+                           },
+                'sortable': [
+                              'emp_id',
+                           ],
+                'date_filters': [
+                   'date_of_joining', 'dob'
+                                ]
+            },
+            'results': data
+        })
+
+
+class CustomLeaveRulesPagination(PageNumberPagination):
+    page = DEFAULT_PAGE
+    page_size = 20
+    page_size_query_param = 'page_size'
+
+    def get_paginated_response(self, data):
+        return Response({
+            'links': {
+                'next': self.get_next_link(),
+                'previous': self.get_previous_link()
+            },
+            'total': self.page.paginator.count,
+            'page': int(self.request.GET.get('page', DEFAULT_PAGE)),
+            'page_size': int(self.request.GET.get('page_size', self.page_size)),
+            'UI_data': {
+                'sticky_headers': [
+                               'emp_id',
+                               'name',
+                                         ],
+                'header': {
+                              'emp_id': 'Employee Id',
+                              'name': 'Employee Name',
+                              "official_email": 'Official Email',
+                              'leave': 'Leave'
+                           },
+                'sortable': [
+                              'emp_id',
+                           ],
+               'date_filters': [
+                   'date_of_joining'
+               ]
+            },
+            'results': data
+        })
+
+class CustomLeaveLogsPagination(PageNumberPagination):
+    page = DEFAULT_PAGE
+    page_size = 20
+    page_size_query_param = 'page_size'
+
+    def get_paginated_response(self, data):
+        return Response({
+            'links': {
+                'next': self.get_next_link(),
+                'previous': self.get_previous_link()
+            },
+            'total': self.page.paginator.count,
+            'page': int(self.request.GET.get('page', DEFAULT_PAGE)),
+            'page_size': int(self.request.GET.get('page_size', self.page_size)),
+            'UI_data': {
+                'sticky_headers': [
+                               'emp_id',
+                               'name',
+                                         ],
+                'header': {
+                              'emp_id': 'Employee Id',
+                              'name': 'Employee Name',
+                              "department": 'Department',
+                              'leave_id': 'Type',
+                              "start_date": 'Start Date',
+                              'end_date': 'End Date',
+                              "days": 'Days',
+                              'status': 'Status',
+                              "action_by": 'Actions',
+                           },
+                'sortable': [
+                              'emp_id',
+                           ],
+               'date_filters': [
+                   'start_date'
+                   'end_date'
+               ]
+            },
+            'results': data
+        })
+
+
+class EmployeeViewSet(viewsets.ModelViewSet):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+
+
+class ListEmployeeViewSet(viewsets.ViewSet):
+    pagination_class = CustomPagination
+    def create(self, request):
+        queryset = Employee.objects.all()
+        serializer = ListEmployeeSerializer(queryset, many=True)
+        if len(queryset) > 0:
+            paginator = CustomPagination()
+            result_page = paginator.paginate_queryset(queryset, request)
+            serializer = ListEmployeeSerializer(result_page, many=True)
+            return paginator.get_paginated_response(serializer.data)
+        else:
+            paginator = CustomPagination()
+            result_page = paginator.paginate_queryset(queryset, request)
+            return paginator.get_paginated_response(result_page)
+
+
+class EmployeeSearchViewSet(viewsets.ModelViewSet):
+
+    serializer_class = ListEmployeeSerializer
+    pagination_class = CustomPagination
+
+    def get_queryset(self, *args, **kwargs):
+        qs = Employee.objects.all()
+        query = self.request.GET.get("keyword")
+        if query:
+            qs = qs.filter(Q(name__contains=query) | Q(
+                emp_id__contains=query) | Q(
+                Permanent_address_line1__contains=query) | Q(
+                designation__contains=query) | Q(
+                gender__contains=query) | Q(
+                official_email__contains=query) | Q(
+                date_of_joining__contains=query) | Q(
+                department__contains=query) | Q(
+                official_number__contains=query) | Q(
+                dob=query) | Q(
+                work_location_add__contains=query))
+
+        query = self.request.GET.get("emp_id")
+        if query:
+            emp_id = query.split(',')
+            qs = qs.filter(emp_id__in=emp_id)
+
+        query = self.request.GET.get("name")
+        if query:
+            name = query.split(',')
+            qs = qs.filter(name__in=name)
+
+        query = self.request.GET.get("Permanent_address_line1")
+        if query:
+            Permanent_address_line1 = query.split(',')
+            qs = qs.filter(Permanent_address_line1__in=Permanent_address_line1)
+
+        query = self.request.GET.get("designation")
+        if query:
+            designation = query.split(',')
+            qs = qs.filter(designation__in=designation)
+
+        query = self.request.GET.get("gender")
+        if query:
+            gender = query.split(',')
+            qs = qs.filter(gender__in=gender)
+
+        query = self.request.GET.get("official_email")
+        if query:
+            official_email = query.split(',')
+            qs = qs.filter(official_email__in=official_email)
+
+        query = self.request.GET.get("date_of_joining")
+        if query:
+            date_of_joining = query.split(',')
+            qs = qs.filter(date_of_joining__in=date_of_joining)
+
+        query = self.request.GET.get("department")
+        if query:
+            department = query.split(',')
+            qs = qs.filter(department__in=department)
+
+        query = self.request.GET.get("official_number")
+        if query:
+            official_number = query.split(',')
+            qs = qs.filter(official_number__in=official_number)
+
+        query = self.request.GET.get("dob")
+        if query:
+            dob = query.split(',')
+            qs = qs.filter(dob__in=dob)
+
+        query = self.request.GET.get("work_location_add")
+        if query:
+            work_location_add = query.split(',')
+            qs = qs.filter(work_location_add__in=work_location_add)
+
+        query = self.request.GET.get("sort_by")
+        if query:
+            sort_key = query
+            if sort_key == 'emp_id':
+                sort_key = 'emp_id'
+            sort_by = ''
+            if 'sort_order' in self.request.data and self.request.data['sort_order'] == 'desc':
+                sort_by = '-'
+            sort_by += sort_key
+            qs = qs.order_by(sort_by)
+
+        return qs
+
+
+class EmployeeColumnViewSet(viewsets.ModelViewSet):
+    # queryset = Employee.objects.all()
+    serializer_class = EmployeeColumnModelSerializer
+    pagination_class = CustomPagination
+
+    def get_queryset(self, *args, **kwargs):
+        qs = Employee.objects.all()
+        query = self.request.GET.get("fields")
+        if query:
+            return qs
+        else:
+            return qs
+
+
+class DocumentsViewSet(viewsets.ModelViewSet):
+    queryset = Documents.objects.all()
+    serializer_class = DocumentsSerializer
+    pagination_class = CustomPagination
+
+
+class FamilyMembersViewSet(viewsets.ModelViewSet):
+    queryset = FamilyMembers.objects.all()
+    serializer_class = FamilyMembersSerializer
+    pagination_class = CustomPagination
+
+
+class EducationViewSet(viewsets.ModelViewSet):
+    queryset = Education.objects.all()
+    serializer_class = EducationSerializer
+    pagination_class = CustomPagination
+
+
+class WorkHistoryViewSet(viewsets.ModelViewSet):
+    queryset = WorkHistory.objects.all()
+    serializer_class = WorkHistorySerializer
+    pagination_class = CustomPagination
+
+
+def ExportEmp(request):
+    response = HttpResponse(content_type='text/csv')
+    write = csv.writer(response)
+    write.writerow(['Employee Id', 'Employee Name', 'Date of Birth', 'Gender', 'Official Email', 'Phone', 'Date of Joining',
+                    'Address', 'Work Location', 'Designation', 'Department'])
+    for employee in Employee.objects.all().values('emp_id', 'name', 'dob', 'gender', 'official_email', 'official_number', 'date_of_joining', 'Permanent_address_line1', 'work_location_add', 'designation', 'department'):
+       write.writerow(employee)
+
+    response['Content-Disposition'] ='attachment; filename="employee.csv"'
+    return response
+
+
+def ExportEmpLeaveLog(request):
+    response = HttpResponse(content_type='text/csv')
+    write = csv.writer(response)
+    write.writerow(['emp_leave_app_id','emp_id', 'leave_id', 'start_date', 'end_date', 'status', 'reason', 'action_by'])
+    for employee in EmpLeaveApplied.objects.all().values('emp_leave_app_id', 'emp_id', 'leave_id', 'start_date', 'end_date', 'status', 'reason', 'action_by'):
+        write.writerow(employee)
+
+    response['Content-Disposition'] = 'attachment; filename="employeeLeaveLog.csv"'
+    return response
+
+
+class LeaveRulesView(viewsets.ModelViewSet):
+    queryset = LeaveRules.objects.all()
+    serializer_class = LeaveRulesSerializer
+
+
+class EmployeeView(viewsets.ModelViewSet):
+    queryset = EmpLeaveId.objects.all()
+    serializer_class = Employee1Serializer
+    pagination_class = CustomLeaveRulesPagination
+
+class AssignRulesSearchViewSet(viewsets.ModelViewSet):
+
+    serializer_class = Employee1Serializer
+    pagination_class = CustomLeaveRulesPagination
+
+    def get_queryset(self, *args, **kwargs):
+        qs = Employee.objects.all()
+        query = self.request.GET.get("keyword")
+        if query:
+            qs = qs.filter(Q(name__contains=query) | Q(emp_id__contains=query) | Q(official_email__contains=query))
+        query = self.request.GET.get("emp_id")
+        if query:
+            emp_id = query.split(',')
+            qs = qs.filter(emp_id__in=emp_id)
+
+        query = self.request.GET.get("name")
+        if query:
+            name = query.split(',')
+            qs = qs.filter(name__in=name)
+        query = self.request.GET.get("official_email")
+        if query:
+            official_email = query.split(',')
+            qs = qs.filter(official_email__in=official_email)
+        query = self.request.GET.get("sort_by")
+        if query:
+            sort_key = query
+            if sort_key == 'emp_id':
+                sort_key = 'emp_id'
+            sort_by = ''
+            if 'sort_order' in self.request.data and self.request.data['sort_order'] == 'desc':
+                sort_by = '-'
+            sort_by += sort_key
+            qs = qs.order_by(sort_by)
+
+        return qs
+
+
+class AssignRulesColumnViewSet(viewsets.ModelViewSet):
+    # queryset = Employee.objects.all()
+    serializer_class = DynamicFieldsModelSerializer
+    pagination_class = CustomLeaveRulesPagination
+
+    def get_queryset(self, *args, **kwargs):
+        qs = Employee.objects.all()
+        query = self.request.GET.get("fields")
+        if query:
+            return qs
+        else:
+            return qs
+
+
+class ListAssignRulesViewSet(viewsets.ViewSet):
+    # pagination_class = CustomPagination
+    def create(self, request):
+        queryset = Employee.objects.all()
+        serializer = ListEmployee1Serializer(queryset, many=True)
+        if len(queryset) > 0:
+            paginator = CustomLeaveRulesPagination()
+            result_page = paginator.paginate_queryset(queryset, request)
+            serializer = ListEmployee1Serializer(result_page, many=True)
+            return paginator.get_paginated_response(serializer.data)
+        else:
+            paginator = CustomLeaveRulesPagination()
+            result_page = paginator.paginate_queryset(queryset, request)
+            return paginator.get_paginated_response(result_page)
+
+
+class EmpLeaveAppliedView(viewsets.ModelViewSet):
+    queryset = EmpLeaveApplied.objects.all()
+    serializer_class = EmpLeaveAppliedSerializer
+
+
+class EmpLogView(viewsets.ModelViewSet):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeenewSerializer
+
+
+class EmployeeLogView(viewsets.ModelViewSet):
+    queryset = EmpLeaveApplied.objects.all()
+    serializer_class = EmpLogSerializer
+
+
+class ListLeaveLogsViewSet(viewsets.ViewSet):
+    # pagination_class = CustomPagination
+    def create(self, request):
+        queryset = Employee.objects.all()
+        serializer = EmployeenewSerializer(queryset, many=True)
+        if len(queryset) > 0:
+            paginator = CustomLeaveLogsPagination()
+            result_page = paginator.paginate_queryset(queryset, request)
+            serializer = EmployeenewSerializer(result_page, many=True)
+            return paginator.get_paginated_response(serializer.data)
+        else:
+            paginator = CustomLeaveLogsPagination()
+            result_page = paginator.paginate_queryset(queryset, request)
+            return paginator.get_paginated_response(result_page)
+
+
+class LeaveLogsSearchViewSet(viewsets.ModelViewSet):
+
+    serializer_class = Employee2Serializer
+    pagination_class = CustomLeaveLogsPagination
+
+    def get_queryset(self, *args, **kwargs):
+        qs = Employee.objects.all()
+        query = self.request.GET.get("keyword")
+        if query:
+            qs = qs.filter(Q(name__contains=query) | Q(emp_id__contains=query) | Q(department__contains=query))
+        query = self.request.GET.get("emp_id")
+        if query:
+            emp_id = query.split(',')
+            qs = qs.filter(emp_id__in=emp_id)
+
+        query = self.request.GET.get("name")
+        if query:
+            name = query.split(',')
+            qs = qs.filter(name__in=name)
+        query = self.request.GET.get("department")
+        if query:
+            department = query.split(',')
+            qs = qs.filter(department__in=department)
+        query = self.request.GET.get("sort_by")
+        if query:
+            sort_key = query
+            if sort_key == 'emp_id':
+                sort_key = 'emp_id'
+            sort_by = ''
+            if 'sort_order' in self.request.data and self.request.data['sort_order'] == 'desc':
+                sort_by = '-'
+            sort_by += sort_key
+            qs = qs.order_by(sort_by)
+
+        return qs
