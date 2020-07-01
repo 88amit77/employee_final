82ac0c0b31e3f2555dfc1e444b9805723fb576e2
vasudev <vasudev.m@pacwisdom.com>
2020-07-01 19:28:18 +0530

    updated serrings.py with django filter backend

--- employees/api/settings.py
+++ employees/api/settings.py
@@ -17,6 +17,7 @@ INSTALLED_APPS = [
 	'api.process',
     'corsheaders',
 	'storages',
+	'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -57,6 +58,9 @@ REST_FRAMEWORK = {
 	'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
 	'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
 	'PAGE_SIZE': 10,
+	'DEFAULT_FILTER_BACKENDS': (
+	    'django_filters.rest_framework.DjangoFilterBackend',
+		),
 }
 
 TEMPLATES = [
