856ca4ca4c0344834f20a7f9251769ddeeebb9c1
amittiwari <88amit77@gmail.com>
2020-08-19 19:26:42 +0530

    process page pagination changed from 10 to 20

--- employees/api/settings.py
+++ employees/api/settings.py
@@ -71,7 +71,7 @@ REST_FRAMEWORK = {
 	'EXCEPTION_HANDLER': 'rest_framework.views.exception_handler',
 	'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
 	'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-	'PAGE_SIZE': 10,
+	'PAGE_SIZE': 20,
 	'DEFAULT_FILTER_BACKENDS': (
 	    'django_filters.rest_framework.DjangoFilterBackend',
 		),
